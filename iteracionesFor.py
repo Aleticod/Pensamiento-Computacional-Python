5ec05ffa579b8fef6475cf954fc12dfe6f71328f
Harvey Unsaac <193837@unsaac.edu.pe>
2022-07-21 00:29:33 +0200

    Added iterations with for

---
+++ iteracionesFor.py
@@ -0,0 +1,12 @@
+def iteracion_for():
+    frutas = ['manzana', 'pera', 'mango']
+    for fruta in frutas:
+        print(fruta)
+
+
+def run():
+    iteracion_for()
+
+
+if __name__ == '__main__':
+    run()
